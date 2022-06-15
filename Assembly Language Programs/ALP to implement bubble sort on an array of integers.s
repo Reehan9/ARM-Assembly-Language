534b103337ac1466452d910738046d92a2b2db42
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-15 18:45:06 +0530

    Add files via upload

---
+++ Assembly Language Programs/ALP to implement bubble sort on an array of integers.s
@@ -0,0 +1,31 @@
+	AREA BUBBLE , CODE
+	ENTRY
+
+	LDR R0 , =n
+	LDRB R1 , [R0]
+	SUB R1 , R1 , #1
+next_pass
+	MOV R2 , R1
+	LDR R3, =ARRAY
+next_comp
+	LDRB R4 , [R3] , #1
+	LDRB R5 , [R3]
+	CMP R4 , R5
+	BLE no_exch
+	
+	STRB R4 , [R3]
+	STRB R5, [R3 , #-1]
+
+no_exch
+	SUBS  R2 ,R2, #1
+	BNE next_comp
+	
+	SUBS R1 , R1 , #1
+	BNE next_pass
+
+GO B GO
+
+	AREA _DATA , DATA
+ARRAY DCB 3,2,1,4,5
+n	SPACE 1
+	END
