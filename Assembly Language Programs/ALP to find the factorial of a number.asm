81de505c1d2f5b09d99bf4c30db8144c0732cad9
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-20 21:13:14 +0530

    Update ALP to find the factorial of a number.asm

--- Assembly Language Programs/ALP to find the factorial of a number.asm
+++ Assembly Language Programs/ALP to find the factorial of a number.asm
@@ -5,7 +5,7 @@
 	MOV R2, #1
 	
 NEXT
-	MUL R2,R1,R2
+	MUL R2,R1,R2 ; Multiply
 	SUB R1,R1,#1
 	CMP R1 ,#1   ; can be substited by SUBS , which is basically a flag indicator in CPSR register  
 	BNE NEXT
