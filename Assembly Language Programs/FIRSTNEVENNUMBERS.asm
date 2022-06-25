c646bf424890b14af6b093d7914c2494136f8bf5
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-25 23:10:12 +0530

    Update FIRSTNEVENNUMBERS.asm

--- Assembly Language Programs/FIRSTNEVENNUMBERS.asm
+++ Assembly Language Programs/FIRSTNEVENNUMBERS.asm
@@ -1,13 +1,14 @@
-     AREA FIRSTTENEVENMEM,CODE
-	 MOV R0,#2	    	;EVEN NUMBERS
-	 MOV R1,#0	    	;COUNTER
-	 MOV R2,#0	    	;SUM
-	 MOV R3,#0x4000000 ;LOAD FIRST ADDRESS IN REGISTER
-NEXT ADD R2,R2,R0   	;ADD EVEN NUMBER TO SUM
-	 ADD R0,R0,#2   	;CALCULATE NEXT EVEN NUMBER
-	 ADD R1,R1,#1   	;INCREEMENT COUNTER
-	 CMP R1,#11     	;TERMINATING CONDITION
-	 BNE NEXT			;BRANCH IF NOT EQUAL
-	 STR R2,[R3]		;USE POSTINDEXING TO STORE VALUE 
-GO   B GO
+     AREA EVEN,CODE
+	 MOV R0,#2	    	
+	 MOV R1,#0 ; Counter variable
+	 MOV R2,#0  ; Result
+	 MOV R3,#0x4000000 ; Pointer towards the memory
+NEXT
+	 ADD R2,R2,R0   	; Addition of even numbers
+	 ADD R0,R0,#2   ; next even number
+	 ADD R1,R1,#1  ; increament of counter variable
+	 CMP R1,#11   ; loop condition
+	 BNE NEXT
+	 STR R2,[R3]	; Store the value in the memory
+OVER	 B OVER
 	 END
