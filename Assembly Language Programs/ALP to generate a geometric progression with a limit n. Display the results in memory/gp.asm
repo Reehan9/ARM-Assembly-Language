43c76f1fe967a7bed45ecc937880c5f858069dd9
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-25 23:02:48 +0530

    Update gp.asm

--- Assembly Language Programs/ALP to generate a geometric progression with a limit n. Display the results in memory/gp.asm
+++ Assembly Language Programs/ALP to generate a geometric progression with a limit n. Display the results in memory/gp.asm
@@ -1,14 +1,14 @@
 	 AREA GEOPROG,CODE
 	 MOV R0,#1
 	 MOV R1,#2
-	 LDR R2,=GP
+	 LDR R2,=GP ; 0x40000000
 	 MOV R3,#10
 NEXT MUL R0,R1,R0
-	 STR R0,[R2],#4
-	 SUB R3,R3,#1 ;
+	 STR R0,[R2],#4 ; increamenting the address by 4 and storing it in memory
+	 SUB R3,R3,#1 
 	 CMP R3,#0
 	 BNE NEXT 
 GO 	 B GO
-	 AREA GEOPROGMEM,DATA
+	 AREA GEOPROGMEM,DATA ; data definition
 GP	 SPACE 10
 	 END
