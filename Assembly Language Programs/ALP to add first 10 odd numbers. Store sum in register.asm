a84fd3a8ed5893297615a9070077c55ed86a56a1
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-25 22:38:25 +0530

    Update and rename add2numbers.asm to ALP to add first 10 odd numbers. Store sum in register.asm

--- Assembly Language Programs/ALP to add first 10 odd numbers. Store sum in register.asm
+++ Assembly Language Programs/ALP to add first 10 odd numbers. Store sum in register.asm
@@ -5,8 +5,8 @@
 NEXT
 	ADD R2 , R2 , R0
 	ADD R0 , R0 , #2 ; TO GET THE NEXT ODD NUMBER
-	ADD R1 , R1 , #1 ; COUNTER
-	CMP R1 , #10
+	ADD R1 , R1 , #1 ; COUNTER variable.
+	CMP R1 , #10 ; Conditional statement
 	BNE NEXT
 GO B GO
 	END
