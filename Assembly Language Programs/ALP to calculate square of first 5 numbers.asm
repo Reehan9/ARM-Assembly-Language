8869dfa52be673bb9ad272db914516fa55f183e4
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-25 23:05:03 +0530

    Update ALP to calculate square of first 5 numbers.asm

--- Assembly Language Programs/ALP to calculate square of first 5 numbers.asm
+++ Assembly Language Programs/ALP to calculate square of first 5 numbers.asm
@@ -2,13 +2,13 @@
 	MOV R0 , #0
 	MOV R1 , #1
 NEXT	
-	BL	SQR
+	BL	SQR ; branch to sqr
 	ADD R0 , R0 , R2 
 	ADD R1 , R1 , #1
 	CMP  R1 , #6
 	BNE NEXT
 GO	B	GO
 SQR	
-	MUL	R2,R1,R1
+	MUL	R2,R1,R1 ; multuply
 	BX	LR ;    MOV PC , LR
 	END
