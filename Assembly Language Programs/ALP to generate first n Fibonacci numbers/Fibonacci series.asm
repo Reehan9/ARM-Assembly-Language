a2f6a333362a46630894993838d78cab3e309892
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-26 09:39:46 +0530

    Update Fibonacci series.asm

--- Assembly Language Programs/ALP to generate first n Fibonacci numbers/Fibonacci series.asm
+++ Assembly Language Programs/ALP to generate first n Fibonacci numbers/Fibonacci series.asm
@@ -5,10 +5,10 @@ start   MOV     R1, #5     ; n = 5
         MOV     R3, #0      ; fn1 = 0
         MOV     R0, #1      ; fn = 1
         MOV     R2, #1      ; curr = 1
-		MOV		R5, #0x40000000
-		STR		R3, [R5] ,#4
+	MOV	R5, #0x40000000
+	STR	R3, [R5] ,#4
 whn     CMP     R2, R1      ; while (curr < 1)
-        STR		R0 , [R5] , #4
+        STR	R0 , [R5] , #4
         BHS     endwhn      ; { ; Branch higher than or same
         ADD     R2, R2, #1  ;   curr = curr + 1
         MOV     R4, R0      ;   tmp = fn
