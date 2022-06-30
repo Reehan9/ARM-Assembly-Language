9ce16d60aaa0d8d7d69b7230c54fc3658c947e08
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-30 20:25:48 +0530

    Create PARB1.c

---
+++ Assembly Language Programs/PARB1.c
@@ -0,0 +1,17 @@
+#include<LPC214X.h>
+void delay(int);
+int main()
+{
+IODIR0 = 0x00000001;
+while(1){
+IOSET0 = 0x00000001;
+delay(500);
+IOCLR0 = 0x00000001;
+delay(500);
+}
+}
+void delay(int n)
+{
+inti =0;
+for(i = 0;i<n;i++);
+}
