224a3101f3a676b3afd04c2c0c5426102a53c304
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-08-08 18:38:03 +0530

    Update ALP to add the first n even numbers. Store the result in memory location.asm

--- Assembly Language Programs/ALP to add the first n even numbers. Store the result in memory location.asm
+++ Assembly Language Programs/ALP to add the first n even numbers. Store the result in memory location.asm
@@ -2,7 +2,7 @@
 	 MOV R0,#2	    	
 	 MOV R1,#0 ; Counter variable
 	 MOV R2,#0  ; Result
-	 MOV R3,#0x4000000 ; Pointer towards the memory
+	 MOV R3,#0x40000000 ; Pointer towards the memory
 NEXT
 	 ADD R2,R2,R0   	; Addition of even numbers
 	 ADD R0,R0,#2   ; next even number
